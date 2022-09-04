d49b52eacff5fb5da78d787f44dc46143fc853b1
Khabran <112776181+khabran@users.noreply.github.com>
2022-09-04 16:04:48 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -27,9 +27,3 @@ def welcome_assignment_answers(question):
         answer = "An error has occurred."
     return answer
 # Complete all the questions.
-
-
-if __name__ == "__main__":
-    # use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
