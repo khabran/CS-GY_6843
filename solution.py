94c6f806ccb6519d8dc172a8d51cb67f01cc2cb8
Khabran <112776181+khabran@users.noreply.github.com>
2022-09-04 14:42:14 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,9 +12,9 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
      elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "Yes/No"
+        answer = "No"
     elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
-        answer = "Yes/no"
+        answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
